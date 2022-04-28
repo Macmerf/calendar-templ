2b17bd475f03844107a62110b001e975ea8a8532
Damir <nuriakhmetov.d.t1998@gmail.com>
2022-04-28 14:33:44 +0300

    calendar 2.0

--- src/components/calendarTempl.vue
+++ src/components/calendarTempl.vue
@@ -1,75 +1,22 @@
 <template>
-
-  <div>{{ date?.toISOString().split("T")[0] }}</div>
-  <div class="calendar-wrapper">
-    <div class="calendar">
-      <div class="custom-month-wrapper" v-if="displayMonths">
-        <div
-          class="custom-month"
-          @click="changeCustomMonth(index)"
-          v-for="(month, index) in customMounths"
-          :key="index * Math.random()"
-          v-bind:class="[currentMonth == index + 1 ? 'active' : '']"
-        >
-          {{ month }}
-        </div>
+  <div class="datepicker-wrapper w-50">
+    <div class="input-date-wrapper" @click="shown = !shown" >
+    <input type="date"  class="form-control"  v-model="date"  readonly />
+    <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M18 2H17V0H15V2H5V0H3V2H2C0.9 2 0 2.9 0 4V20C0 21.1 0.9 22 2 22H18C19.1 22 20 21.1 20 20V4C20 2.9 19.1 2 18 2ZM18 20H2V9H18V20ZM18 7H2V4H18V7Z" fill="#4E5256"/>
+    </svg>
+  </div>
+    <div v-if="shown" class="calendar-wrapper">
+      <div class="calendar">
+        <DatePicker v-model="date" color="#E24C3B" :model-config="modelConfig">
+        </DatePicker>
+        <button @click="shown = !shown" class="custom-calendar-cancel">
+          Cancel
+        </button>
+        <button @click="shown = !shown" class="custom-calendar-set-date">
+          Set Date
+        </button>
       </div>
-      <div class="custom-year-wrapper" v-if="displayYears">
-        <div
-          class="custom-year"
-          @click="changeCustomYear(year)"
-          v-for="(year, index) in customYears"
-          :key="index * Math.random()"
-          v-bind:class="[currentYear == year ? 'active' : '']"
-        >
-          {{ year }}
-        </div>
-      </div>
-      <button class="open-months" @click="displayMonths = !displayMonths">
-        {{ customMounths[currentMonth - 1] }}
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0.75 0.75L5 5.25L9.25 0.75"
-            stroke="#3F3A38"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </button>
-      <button class="open-years" @click="displayYears = !displayYears">
-        {{ currentYear }}
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0.75 0.75L5 5.25L9.25 0.75"
-            stroke="#3F3A38"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </button>
-      <DatePicker
-        v-model="date"
-        highlight.style="font-weght: 400"
-        color="#E24C3B"
-        ref="calendar"
-      >
-      </DatePicker>
-      <button class='custom-calendar-cancel'>Cancel</button>
-      <button class='custom-calendar-set-date'>Set Date</button>
     </div>
   </div>
 </template>
@@ -85,44 +32,15 @@ export default {
   },
   data() {
     return {
-      date: new Date(),
-      customMounths: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ],
-      customYears: [
-        2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032,
-      ],
-      currentMonth: new Date().getMonth() + 1,
-      currentYear: new Date().getFullYear(),
-      displayMonths: false,
-      displayYears: false,
+      date: new Date().toLocaleDateString(),
+      shown: false,
+      modelConfig: {
+        type: "string",
+        mask: "YYYY-MM-DD", // Uses 'iso' if missing
+      },
     };
   },
-  methods: {
-    changeCustomMonth: async function (index) {
-      const calendar = this.$refs.calendar;
-      this.currentMonth = index + 1;
-      this.displayMonths = !this.displayMonths;
-      await calendar.move({ month: index + 1, year: this.currentYear });
-    },
-    changeCustomYear: async function (year) {
-      const calendar = this.$refs.calendar;
-      this.currentYear = year;
-      this.displayYears = !this.displayYears;
-      await calendar.move({ month: this.currentMonth, year: year });
-    },
-  },
+  methods: {},
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.scss'
-
+import './assets/inputs.scss'
+import './assets/_vars-dark.scss'
+import './assets/_vars.scss'
 createApp(App).mount('#app')
